3970f4f5f9ec29b143944d690be5d2f515106fbd
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-06-30 21:36:19 +0300

    complite webpack settings

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/js/main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  mode: "development",
+  devServer: {
+    static: "./dist",
+    open: true,
+    hot: true,
+  },
+};
